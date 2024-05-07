672b8b34b2a057f7d0d506fe2e1adea868550ade
sami <sami.omar.7345@gmail.com>
2024-05-08 00:23:52 +0200

    commit 1

---
+++ InventoryApp-widerholung/InventoryApp_widerholungApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryApp_widerholungApp.swift
+//  InventoryApp-widerholung
+//
+//  Created by Sami on 07.05.24.
+//
+
+import SwiftUI
+
+@main
+struct InventoryApp_widerholungApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
